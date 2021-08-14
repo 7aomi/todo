c99e029f60f85602c51aa637172fc9bbb9856c64
naomium <88881401+naomium@users.noreply.github.com>
2021-08-15 00:42:46 +0900

    todoリストの完成

--- src/index.js
+++ src/index.js
@@ -3,13 +3,24 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   // 値を初期化する
   document.getElementById("add-text").value = "";
+
+  createIncompleteList(inputText);
+};
+
+// 未完了リストから指定のTODOを削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+// 未完了のTODOリストに追加する関数
+const createIncompleteList = (text) => {
   // divタグの生成
   const div = document.createElement("div");
   div.className = "list-row";
 
   // liタグの生成
   const li = document.createElement("li");
-  li.innerText = inputText;
+  li.innerText = text;
 
   // button（完了）タグ作成
   const completeButton = document.createElement("button");
@@ -17,6 +28,38 @@ const onClickAdd = () => {
   completeButton.addEventListener("click", () => {
     // クリックされた削除ボタンの親要素（div）を未完了リストから削除
     deleteFromIncompleteList(completeButton.parentNode);
+
+    // 完了したリストに追加
+    const addTarget = completeButton.parentNode;
+    // TODO内容テキストを取得
+    const text = addTarget.firstElementChild.innerText;
+
+    // div以下を初期化
+    addTarget.textContent = null;
+
+    // liタグを生成
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    // 戻すbuttonタグ生成
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      // クリックされた戻すボタンの親要素のdivを完了リストから削除
+      const deleteTarget = backButton.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+
+      // テキストを取得
+      const text = backButton.parentNode.firstElementChild.innerText;
+      createIncompleteList(text);
+    });
+
+    // divタグの子要素に各要素を設定
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton);
+
+    // 完了リストに追加
+    document.getElementById("complete-list").appendChild(addTarget);
   });
 
   // button（削除）タグ作成
@@ -34,11 +77,6 @@ const onClickAdd = () => {
 
   // 未完了リストに追加
   document.getElementById("incomplete-list").appendChild(div);
-};
-
-// 未完了リストから指定のTODOを削除
-const deleteFromIncompleteList = (target) => {
-  document.getElementById("incomplete-list").removeChild(target);
 };
 
 document
